0c59c3c4e4a354d5040387a91c3f4053f147186f
Kezi Rose <keziahflorence@gmail.com>
2022-03-14 13:51:19 +0100

    seller auth fe and be

--- client/src/App.js
+++ client/src/App.js
@@ -10,10 +10,13 @@ import Checkout from './pages/Checkout';
 import Home from './pages/Home';
 import UserLogin from './pages/UserLogin';
 import UserSignUp from './pages/UserSignUp';
+import SellerLogin from './pages/SellerLogin';
+import SellerSignUp from './pages/SellerSignUp';
 import ErrorPage from "./pages/ErrorPage";
 
 function App() {
   const [user, setUser] = useState(Local.getUser());
+  const [seller, setSeller] = useState(Local.getSeller());
   const [loginError, setLoginError] = useState('');
   const navigate = useNavigate();
   // const history = useHistory();
@@ -32,14 +35,44 @@ function App() {
     }
   }
 
+  async function handleSellerLogin(username, password) {
+    let response = await Api.loginSeller(username, password);
+    console.log(response)
+    if (response.ok) {
+        Local.saveSellerInfo(response.data.token, response.data.seller);
+        setSeller(response.data.seller);
+        setLoginError('');
+        navigate('/');
+    } else {
+        setLoginError(response.error);
+        console.log(loginError);
+    }
+  }
+
   async function handleUserLogout() {
     Local.removeUserInfo();
     setUser(Local.getUser());
+  }
+
+  async function handleSellerLogout() {
+    Local.removeSellerInfo();
+    setSeller(Local.getSeller());
   }
   
 
   const handleUserSignUp = async (newUser) => {
     let response = await Api.userSignUp(newUser.username, newUser.password, newUser.email);
+    if (response.ok) {
+      setLoginError('');
+      console.log("Sign up successful!")
+  } else {
+      setLoginError(response.error);
+      console.log(loginError);
+  }
+  }
+
+  const handleSellerSignUp = async (newSeller) => {
+    let response = await Api.sellerSignUp(newSeller.username, newSeller.password, newSeller.email);
     if (response.ok) {
       setLoginError('');
       console.log("Sign up successful!")
@@ -64,6 +97,19 @@ function App() {
     }
   }
 
+  async function updateSellerData(sellerObj, route) {
+    let response = await Api.updateSellerData(sellerObj, route);
+    console.log(response)
+    if (response.ok) {
+        Local.saveSellerInfo(response.data.token, response.data.seller);
+        setSeller(response.data.seller);
+        setLoginError('');
+    } else {
+        setLoginError('Login failed');
+        console.log(loginError);
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -72,6 +118,8 @@ function App() {
           <Route path="/checkout" element={<Checkout />} />
           <Route path="/user-login" element={<UserLogin userLogInCb={(username, password) => handleUserLogin(username, password)}/>} loginError={loginError}/>
           <Route path="/user-signup" element={<UserSignUp addUserCb={(newUser) => handleUserSignUp(newUser)} />} />
+          <Route path="/seller-login" element={<SellerLogin sellerLogInCb={(username, password) => handleSellerLogin(username, password)}/>} loginError={loginError}/>
+          <Route path="/seller-signup" element={<SellerSignUp addSellerCb={(newSeller) => handleSellerSignUp(newSeller)} />} />
         </Routes>
       </header>
     </div>

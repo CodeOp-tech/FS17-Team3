6b89428334a143b719751ce24af9cde37d4b9072
Ellie Foote <eleanorjfoote@gmail.com>
2022-03-18 16:44:45 +0100

    navbar and sidebar position

---
+++ client/src/components/Sidebar.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Sidebar() {
+     return (
+          <nav className="Navbar">
+               <p>Sidebar placeholder</p>
+          </nav>
+     );
+}
+export default Sidebar;

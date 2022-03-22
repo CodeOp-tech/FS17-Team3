f2cd72e53fc15ab189a7e5749aafa149dc0d3863
elliefoote <86239914+elliefoote@users.noreply.github.com>
2022-03-22 10:58:09 +0100

    Revert "test"

--- routes/sellers.js
+++ routes/sellers.js
@@ -3,8 +3,6 @@ var router = express.Router();
 const {ensureSameSeller} = require('../middleware/guards');
 const {makePatchSQL} = require('../middleware/helper');
 const db = require("../model/helper");
-const fs = require('fs/promises');
-var path = require('path');
 
 /* GET sellers listing. */
 router.get('/', function(req, res, next) {
@@ -40,45 +38,7 @@ router.patch('/:sellerid', ensureSameSeller, async (req, res) => {
   }
 });
 
-/* Patch profile image
 
-const mime = require("mime-types");
-const { imagefile } = req.files;
-const extension = mime.extension(imagefile.mimetype);
-const filename1 = filename1 + "." + extension;
-
-- Create a folder in public called images/sellers.
-- Store just the filename in the DB and store the image in the folder above
-- In the GET /sellers/:id route, after fetching the record from the DB, prepend the filename in profilepic with something like http://localhost:5000/images/sellers so it’s a proper URL that the client can use*/
-
-router.patch('/profile/:sellerid', ensureSameSeller, async function (req, res) {
-  let { sellerid } = req.params.sellerid;
-  console.log("req.files:", req.files)
-  // ^ shows roses.jpeg but problem with myfile.temp. Changeto req.file and myfile error fixed but req.file undfined 400 no file
-  if(!req.files || Object.keys(req.files).length === 0) {
-    res.status(400).send({error: 'No file'})
-    return;
-  }
-  let { myfile } = req.files;
-  let fromPath = myfile.tempFilePath;
-  let toPath = path.join(__dirname, '../public/images/sellers/') + myfile.name;
-  try {
-    await fs.rename(fromPath, toPath);
-    let sql = `
-    INSERT INTO sellers (picurl)
-    VALUES ('${myfile.name}')
-    WHERE sellerid = ${sellerid}
-    `;
-    await db(sql)
-    res.status(201).send('Profile photo successfully updated!');
-  } catch (err) {
-    res.status(500).send({error: err.message});
-  }
-})
-
-// let result = await db(sql);
-//     let seller = result.data[0];
-//     res.status(201).send(seller);
 
 
 // GET all products where listedby = sellerid

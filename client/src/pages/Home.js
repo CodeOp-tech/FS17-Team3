e26ab4babf90fb117c0dcf44d8e6c763848dd421
stabash <natashareedbrewer@googlemail.com>
2022-03-22 14:11:38 +0100

    photo and buttons added to homepage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,25 +2,27 @@ import React from "react";
 import "../App.css";
 import { Link } from "react-router-dom";
 
-function Home() {
+function Home({ user, seller }) {
      return (
           <div className="container">
-               <h1>Homegrown</h1>
-               <div className="">
-                    <h2>
-                         <Link className="btn btn-primary me-3" to="/signup">
-                              Create New Account
-                         </Link>
-                         <Link className="btn btn-primary" to="/login">
-                              Log In
-                         </Link>
-                    </h2>
-                    <body>
-                         <img
-                              src="https://images.pexels.com/photos/4153232/pexels-photo-4153232.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
-                              alt="Girl Writing At Desk"
-                         ></img>
-                    </body>
+               <h1>
+                    Homegrown{" "}
+                    <Link className="btn btn-primary me-3" to="/signup">
+                         Create New Account
+                    </Link>
+                    <Link className="btn btn-primary" to="/login">
+                         Log In
+                    </Link>
+               </h1>
+               <h3></h3>
+
+               <div className=""></div>
+               <div>
+                    <img
+                         src="https://images.pexels.com/photos/4153232/pexels-photo-4153232.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+                         alt="Girl Writing At Desk"
+                         width="1050px"
+                    ></img>
                </div>
           </div>
      );

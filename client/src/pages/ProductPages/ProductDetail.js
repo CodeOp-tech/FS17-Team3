be48d956cd324c15263cd8a6bf7c0a4021f37ca7
Ellie Foote <eleanorjfoote@gmail.com>
2022-03-22 15:21:29 +0100

    prodc detail functions

--- client/src/pages/ProductPages/ProductDetail.js
+++ client/src/pages/ProductPages/ProductDetail.js
@@ -1,7 +1,8 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useState, useContext} from "react";
 import {useParams, Link} from "react-router-dom";
 import Api from '../../helpers/Api';
 import './ProductDetail.css';
+import CartContext from '../../CartContext';
 
 function ProductDetail() {
     const {productid} = useParams();
@@ -22,6 +23,19 @@ function ProductDetail() {
           }
         };
 
+        let { cart, addToCartCB, increaseOrderCountCB, decreaseOrderCountCB } = useContext(CartContext); 
+
+        const handleAdd = (id, price) => {
+            addToCartCB(id, price);
+          }
+    
+        const handleIncrease = (id, current, price) => {
+            increaseOrderCountCB(id, current, price);
+        }
+        
+        const handleDecrease = (id, current, price) => {
+            decreaseOrderCountCB(id, current, price);
+        }
 
     return (
           <div className="container product-detail-page d-flex flex-column align-items-center">
@@ -43,7 +57,22 @@ function ProductDetail() {
                         <div className="highlight-listedby">Listed by: <Link to={`/shop/${highlightedProduct.listedby}`} >{highlightedProduct.username}</Link></div>
                         <div className="highlight-name fs-2">{highlightedProduct.name}</div>
                         <div className="fs-3">€{(highlightedProduct.price/100).toFixed(2)}</div>
-                        <div><button className="btn btn-add-to-cart fs-5 px-3 mt-3 mb-3">Add to Cart</button></div>
+                        
+                        {   
+                            (cart.filter(cartitem => cartitem.productid === highlightedProduct.productid).length) > 0
+                            ?
+                            (
+                                <div className="d-flex fs-5 mt-3 mb-3">
+                                <button className="btn btn-add-to-cart fs-5 px-3" disabled>Product Added!</button>
+                                <button className="btn btn-sm ms-2 btn-cart-left text-light" onClick={e => handleIncrease(highlightedProduct.productid, cart.filter(cartitem => cartitem.productid === highlightedProduct.productid)[0].quantity, highlightedProduct.price)}> + </button>
+                                <div className="cart-quantity d-flex align-items-center justify-content-center">{cart.filter(cartitem => cartitem.productid === highlightedProduct.productid)[0].quantity}</div>
+                                <button className="btn btn-sm btn-cart-right text-light" onClick={e => handleDecrease(highlightedProduct.productid, cart.filter(cartitem => cartitem.productid === highlightedProduct.productid)[0].quantity, highlightedProduct.price)}>-</button>
+                                </div>
+                            )
+                            :
+                            (<button className="btn btn-add-to-cart fs-5 px-3 mt-3 mb-3" onClick={e => handleAdd(highlightedProduct.productid, highlightedProduct.price)}>Add to Cart</button>)
+                        }
+                                            
                         <div className="highlight-description">{highlightedProduct.description}</div>
                         
                    
